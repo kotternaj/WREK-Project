c1d61fcdb632e89f0f55e1b8bcd6b24f13d8f8fc
kotternaj <jreddock@gmail.com>
2020-06-29 17:32:30 -0400

    project setup

---
+++ shows/show.py
@@ -0,0 +1,83 @@
+Not Just Jazz
+https://www.wrek.org/playlist.php/main/128kbs/current/NJJ.m3u
+ 
+12pm
+North Avenue Lounge
+https://www.wrek.org/playlist.php/main/128kbs/current/NAL.m3u
+ 
+6pm
+Battleground
+https://www.wrek.org/playlist.php/main/128kbs/current/BTTG.m3u
+ 
+7pm
+NO!
+https://www.wrek.org/playlist.php/main/128kbs/current/NO.m3u
+ 
+8:00 PM
+Shillelagh Law
+https://www.wrek.org/playlist.php/main/128kbs/current/SLAW.m3u
+ 
+9:00 PM
+Sub-Saharan Vibes
+https://www.wrek.org/subsaharanvibes/
+ 
+10:00 PM
+Slow Riot
+https://www.wrek.org/playlist.php/main/128kbs/current/SLOW.m3u
+ 
+Monday
+ 
+12am
+Somnia
+https://www.wrek.org/playlist.php/main/128kbs/current/SOM.m3u
+ 
+12pm
+Freaker's Ball
+https://www.wrek.org/playlist.php/main/128kbs/current/FKBL.m3u
+ 
+5:00 PM
+Tapas Con Tumbao
+https://www.wrek.org/playlist.php/main/128kbs/current/TCTB.m3u
+ 
+6:00 PM
+Goldsoundz
+https://www.wrek.org/playlist.php/main/128kbs/current/GS.m3u
+ 
+7:00 PM
+Girl Rock!
+https://www.wrek.org/playlist.php/main/128kbs/current/GIRL.m3u
+ 
+8:00 PM
+New Forces
+https://www.wrek.org/playlist.php/main/128kbs/current/NF.m3u
+ 
+9:00 PM
+The Longboards Show
+https://www.wrek.org/playlist.php/main/128kbs/current/TLBS.m3u
+ 
+10:00 PM
+Live at WREK
+https://www.wrek.org/playlist.php/main/128kbs/current/LAW.m3u
+ 
+11:00 PM
+Underground Recordings
+https://www.wrek.org/playlist.php/main/128kbs/current/UGR.m3u
+
+# for show in shows:
+#     show_m3u = shows[show]
+#     print(show_m3u)
+
+# showlist = list(shows)
+# print(showlist[2])
+
+# url1, url2 = format_url(url)
+# print(url1 + "\n" + url2)
+
+# # extract the file names from the URL path
+# mp3_name1 = get_filename_from_path(url1)
+# mp3_name2 = get_filename_from_path(url2)
+
+# download(url1, mp3_name1)
+# download(url2, mp3_name2)
+
+# print(mp3_name1 + "\n" + mp3_name2)
---
+++ shows/wrek.py
@@ -0,0 +1,59 @@
+from urllib.parse import urlsplit, urlparse
+import urllib
+import urllib.request
+import os, time
+import datetime
+from datetime import date
+from shows import shows
+
+def main():
+    for show in shows:
+        show_name = str(show)
+        url = shows[show]
+        year = current_year()
+        week = str("Week " + current_week())
+        save_path = "Users\Owner\Projects\wrek-project\shows\show_data"
+        complete_path = os.path.join(save_path, year, show_name, week)
+
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            html = html.decode("utf-8")
+            # split the M3U (playlist) into seperate URLs
+            split_urls = html.split('\n')
+            print(split_urls)
+            # isolate the mp3 name and use it as the save name
+            for urls in split_urls:
+                if urls == '':
+                    pass
+                else:
+                    sep_urls = urlparse(urls)
+                    mp3_filename = os.path.basename(sep_urls[2])
+                    download(urls, mp3_filename, complete_path)
+
+def download(urls, mp3_filename, complete_path):
+    if os.path.exists(complete_path):
+        print('Directory "%s" already exists' %complete_path)
+    else:
+        os.makedirs(complete_path)
+        print('New directory "%s" was created' %complete_path)
+    os.chdir(complete_path)
+    try:
+        print('Saving file: ', mp3_filename)
+        urllib.request.urlretrieve(urls, mp3_filename)
+        print('Successfully saved: ', mp3_filename)
+    except(ValueError):
+        pass
+
+# determine current week to be used to name the auto-generated dirs
+def current_week():
+    y, m, d = date.today().year, date.today().month, date.today().day
+    week_num = datetime.date(y, m, d).isocalendar()[1]
+    week = str(week_num)
+    return week
+
+def current_year():
+    year_ = str(date.today().year)
+    return year_
+
+if __name__ == '__main__':
+    main()
