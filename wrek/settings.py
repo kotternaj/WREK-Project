638d10297c6cfb209c879c5240d205c9b24aab40
kotternaj <jreddock@gmail.com>
2020-07-01 16:03:00 -0400

    generated new secret key

--- wrek/settings.py
+++ wrek/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from secret_settings import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nyq2k%dp(fg%_h=&_&dp$tnd3)*1qsqk&9pkjgh3a_l*fqei=b'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
