bf62932025e4d05b6f14891ddc6541e851352f14
kotternaj <jreddock@gmail.com>
2020-07-09 10:18:58 -0400

    refactored

--- shows/get_show_url.py
+++ shows/get_show_url.py
@@ -18,48 +18,32 @@ def list_blobs(bucket_name):
     # print(url_dict)
     return(public_urls)
 
-def get_mode7_urls():
+def find_show_week(showname):
     public_urls = list_blobs('wrek-01')
-    mode7_urls = []
+    mp3_urls = []
     week = []
     filename = []
     for url in public_urls:
         filepath  = os.path.join(url)
         filepath = os.path.normpath(filepath)
         filepath = filepath.split(os.sep)
-        # remove everything up to /<show name>/<week>/>
-        # and then join above items back into a path
-        del filepath[0:3]
-        week = filepath[1]
-        print(filepath)
-        if filepath[0] == 'Mode7':
-            filename.append(filepath[2])
-            print(filename)
-            mode7_urls.append(url)
-    return(mode7_urls, week, filename)
+
+        del filepath[0:3] #/<showname>/<week>/<mp3>
+        week = filepath[1] #grab week
+        # print(filepath)
+
+        if filepath[0] == showname: # if /showname/x/x == showname
+            for file in filepath:
+                filename.append(filepath[2])
+                print(filename)
+                mp3_urls.append(url)
+                print(mp3_urls)
+            return(mp3_urls, week, filename)
         # upload_file_path = os.path.join(*filepath).replace("\\","/")
 
 
-    # for url in public_url:
-    #     str(url)
-    #     print(url)
-    # for k, v in url_dict.items():
-    #     if k.startswith(k):
-    #         key_list.append(k)
-    #         print(key_list)
 
-
-    # showpath = blob.name
-    # show_name = showpath.split('/')
-    # show = show_name[0]
-    # if show_name[0] == show:
-    #     pass
-    # print(show_name)
-    # print(show)
-        # print(public_url)
-    # return(two_urls)
-    # two_urls = public_url[:2]
     # print(two_urls)
 
 if __name__ == '__main__':
-    get_mode7_urls()
+    find_show_week('Mode7')
--- shows/views.py
+++ shows/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .shows_dict import shows
 from .current_week import current_week
 from .upload import upload_to_gcs
-from .get_show_url import list_blobs, get_mode7_urls
+from .get_show_url import list_blobs, find_show_week
 
 def home(request):
     context = {'shows': shows}
@@ -11,14 +11,12 @@ def home(request):
 
 def mode7(request):
     # weeks = [i for i in (range(20, current_week() + 1))]
-    urls, week, filename = get_mode7_urls()
+    urls, week, filename = find_show_week(shows)
     url1 = urls[0]
-    print(url1)
     filename1 = filename[0]
     url2 = urls[1]
-    print(filename1)
     filename2 = filename[1]
-    print(filename2)
+
     context = {'week': week, 'url1': url1, 'url2': url2,
                'filename1': filename1, 'filename2': filename2}
     return render(request, 'shows/mode7.html', context)
