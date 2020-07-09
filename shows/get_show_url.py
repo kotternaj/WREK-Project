4234ec73ff7722e357ef762e716d1ffadcd1f012
kotternaj <jreddock@gmail.com>
2020-07-09 13:36:06 -0400

    fixed append show to list based on name

--- shows/get_show_url.py
+++ shows/get_show_url.py
@@ -5,40 +5,43 @@ bucket_url: 'https://storage.googleapis.com/wrek-01/'
 
 def list_blobs(bucket_name):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/JSON/WREK-01.json"
-
     storage_client = storage.Client()
     blobs = storage_client.list_blobs(bucket_name)
     public_urls = []
-    # url_dict = {}
-    # key_list = []
 
     for blob in blobs:
-        # url_dict.update({blob.name : blob.public_url})
-        public_urls.append(blob.public_url)
-    # print(url_dict)
+        public_urls.append(blob.public_url)    
     return(public_urls)
 
-def find_show_week(showname):
+def find_show_url(showname):
     public_urls = list_blobs('wrek-01')
-    mp3_urls = []
-    week = []
+    print(public_urls)
+    show_urls = []
     filename = []
+    file = []
+    weeks = []
     for url in public_urls:
         filepath  = os.path.join(url)
         filepath = os.path.normpath(filepath)
+
         filepath = filepath.split(os.sep)
 
         del filepath[0:3] #/<showname>/<week>/<mp3>
-        week = filepath[1] #grab week
-        # print(filepath)
 
-        if filepath[0] == showname: # if /showname/x/x == showname
-            for file in filepath:
-                filename.append(filepath[2])
-                print(filename)
-                mp3_urls.append(url)
-                print(mp3_urls)
-            return(mp3_urls, week, filename)
+        file = filepath[2] #ex. Sun1800.mp3
+
+        if filepath[0] == showname:
+            print('show name DOES match show name in URL')
+            show_urls.append(url)
+            weeks.append(filepath[1]) # grab week number
+            filename.append(file)
+        else:
+            print('show name does NOT match')
+
+    print(show_urls)
+
+
+    return(show_urls, weeks, filename)
         # upload_file_path = os.path.join(*filepath).replace("\\","/")
 
 
@@ -46,4 +49,4 @@ def find_show_week(showname):
     # print(two_urls)
 
 if __name__ == '__main__':
-    find_show_week('Mode7')
+    find_show_url('Mode7')
--- shows/views.py
+++ shows/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .shows_dict import shows
 from .current_week import current_week
 from .upload import upload_to_gcs
-from .get_show_url import list_blobs, find_show_week
+from .get_show_url import list_blobs, find_show_url
 
 def home(request):
     context = {'shows': shows}
@@ -11,14 +11,14 @@ def home(request):
 
 def mode7(request):
     # weeks = [i for i in (range(20, current_week() + 1))]
-    urls, week, filename = find_show_week(shows)
-    url1 = urls[0]
-    filename1 = filename[0]
-    url2 = urls[1]
-    filename2 = filename[1]
+    urls, weeks, mp3s = find_show_url('Mode7')
+    print(weeks)
+    # url1, url2 = urls
+    # mp3_00, mp3_30 = filenames
+    # url2 = urls[1]
+    # filename2 = filename[1]
 
-    context = {'week': week, 'url1': url1, 'url2': url2,
-               'filename1': filename1, 'filename2': filename2}
+    context = {'weeks': weeks, 'urls': urls, 'mp3s': mp3s}
     return render(request, 'shows/mode7.html', context)
 
 def goldsoundz(request):
