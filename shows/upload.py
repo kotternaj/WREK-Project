a252252878531e780b4394348728ae0f3560fdcc
kotternaj <jreddock@gmail.com>
2020-07-14 12:10:05 -0400

    code cleanup

--- shows/upload.py
+++ shows/upload.py
@@ -9,11 +9,8 @@ def upload_to_gcs(filepath, file):
     blob = bucket.blob(f"{filepath}/{file}")
     blob.upload_from_filename(file)
     url = blob.public_url
-    # return(url)
     print(f"MP3 URL - {url}")
-    # mp3_urls.append(url)
-    # return(mp3_urls)
-    # m3u = create_m3u(mp3_urls, filepath)
+
 
 if __name__ == '__main__':
     upload_to_gcs('Mode7/30', 'playlist.m3u')
--- shows/views.py
+++ shows/views.py
@@ -11,20 +11,7 @@ def home(request):
 
 def mode7(request):
     playlist_links = get_m3u_link('Mode7')
-    # mp3_list = filename[0], filename[1]
-    # print(url_dict)
-    # for x in url_dict[0]:
-    #     print(x)
-    # weeks = [i for i in (range(20, current_week() + 1))]
-    # urls, weeks, mp3s = find_show_url('Mode7')
-    # url1, url2 = urls
-    # mp3_00, mp3_30 = filename
-    # url2 = urls[1]
-    # filename2 = filename[1]
-    # print(mp3_list)
-    context = {'playlist_links': playlist_links }
-    # context = {'url_dict': url_dict, 'mp3_00': mp3_00, 'mp3_30': mp3_30 }
-    # context = {'weeks': weeks, 'urls': urls, 'mp3s': mp3s}
+    context = {'playlist_links': playlist_links }    
     return render(request, 'shows/mode7.html', context)
 
 def goldsoundz(request):
