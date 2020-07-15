ef2459d65330ad06a34f616468a11a294d27305f
kotternaj <jreddock@gmail.com>
2020-07-15 15:24:28 -0400

    styled shows list on home page

--- shows/show.py
+++ shows/show.py
@@ -1,64 +1,73 @@
+# 'WREKage' : 'https://www.wrek.org/playlist.php/main/128kbs/previous/WRKG.m3u',
+# 'Not Just Jazz' : 'https://www.wrek.org/playlist.php/main/24kbs/current/NJJ.m3u',
+# 'North Avenue Lounge' : 'https://www.wrek.org/playlist.php/main/24kbs/previous/NAL.m3u',
+# 'Battleground' : 'https://www.wrek.org/playlist.php/main/128kbs/current/BTTG.m3u',
+# 'NO!': 'https://www.wrek.org/playlist.php/main/128kbs/current/NO.m3u',
+# 'Shillelagh Law' : 'https://www.wrek.org/playlist.php/main/128kbs/current/SLAW.m3u',
+# 'Sub Saharan Vibes' : 'https://www.wrek.org/playlist.php/main/128kbs/current/SSV.m3u',
+# 'Slow Riot' : 'https://www.wrek.org/playlist.php/main/128kbs/previous/SLOW.m3u'
+
 Not Just Jazz
 https://www.wrek.org/playlist.php/main/128kbs/current/NJJ.m3u
- 
+
 12pm
 North Avenue Lounge
 https://www.wrek.org/playlist.php/main/128kbs/current/NAL.m3u
- 
+
 6pm
 Battleground
 https://www.wrek.org/playlist.php/main/128kbs/current/BTTG.m3u
- 
+
 7pm
 NO!
 https://www.wrek.org/playlist.php/main/128kbs/current/NO.m3u
- 
+
 8:00 PM
 Shillelagh Law
 https://www.wrek.org/playlist.php/main/128kbs/current/SLAW.m3u
- 
+
 9:00 PM
 Sub-Saharan Vibes
 https://www.wrek.org/subsaharanvibes/
- 
+
 10:00 PM
 Slow Riot
 https://www.wrek.org/playlist.php/main/128kbs/current/SLOW.m3u
- 
+
 Monday
- 
+
 12am
 Somnia
 https://www.wrek.org/playlist.php/main/128kbs/current/SOM.m3u
- 
+
 12pm
 Freaker's Ball
 https://www.wrek.org/playlist.php/main/128kbs/current/FKBL.m3u
- 
+
 5:00 PM
 Tapas Con Tumbao
 https://www.wrek.org/playlist.php/main/128kbs/current/TCTB.m3u
- 
+
 6:00 PM
 Goldsoundz
 https://www.wrek.org/playlist.php/main/128kbs/current/GS.m3u
- 
+
 7:00 PM
 Girl Rock!
 https://www.wrek.org/playlist.php/main/128kbs/current/GIRL.m3u
- 
+
 8:00 PM
 New Forces
 https://www.wrek.org/playlist.php/main/128kbs/current/NF.m3u
- 
+
 9:00 PM
 The Longboards Show
 https://www.wrek.org/playlist.php/main/128kbs/current/TLBS.m3u
- 
+
 10:00 PM
 Live at WREK
 https://www.wrek.org/playlist.php/main/128kbs/current/LAW.m3u
- 
+
 11:00 PM
 Underground Recordings
 https://www.wrek.org/playlist.php/main/128kbs/current/UGR.m3u
